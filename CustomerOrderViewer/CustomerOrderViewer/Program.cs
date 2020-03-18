2c48031692b458b289070a1166a6f2e9a96ae3ec
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-03-18 23:16:25 +0100

    CustomerOrderViewer is ready. Simple version, but shows the SQL View in a console.

--- CustomerOrderViewer/CustomerOrderViewer/Program.cs
+++ CustomerOrderViewer/CustomerOrderViewer/Program.cs
@@ -3,6 +3,7 @@ using CustomerOrderViewer.Repository;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient; //Nuget csomag
+using System.Linq;
 
 namespace CustomerOrderViewer
 {
@@ -10,9 +11,29 @@ namespace CustomerOrderViewer
     {
         static void Main(string[] args)
         {
-            CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=.\sqlexpress;Initial Catalog=CustomerOrderViewer;Integrated Security=True");
+            try
+            {
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=.\sqlexpress;Initial Catalog=CustomerOrderViewer;Integrated Security=True");
 
-            IList<CustomerOrderDetailModel> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+                IList<CustomerOrderDetailModel> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+
+                if (customerOrderDetailModels.Any())
+                {
+                    foreach (CustomerOrderDetailModel customerOrderDetailModel in customerOrderDetailModels)
+                    {
+                        Console.WriteLine($"{customerOrderDetailModel.CustomerOrderId}:" +
+                            $" Fullname: {customerOrderDetailModel.FirstName} {customerOrderDetailModel.LastName }" +
+                            $" (Id: {customerOrderDetailModel.CustomerId})" +
+                            $"- purchased {customerOrderDetailModel.Description}" +
+                            $" for {customerOrderDetailModel.Price}" +
+                            $" (Id: {customerOrderDetailModel.ItemId})"); 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong: {ex.Message}");
+            }
         }
     }
 }
