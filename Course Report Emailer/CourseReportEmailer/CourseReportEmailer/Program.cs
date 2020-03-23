b810f25e8184495f65736442876bf1eac861ffe8
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-03-23 10:51:29 +0100

    DataTable

--- Course Report Emailer/CourseReportEmailer/CourseReportEmailer/Program.cs
+++ Course Report Emailer/CourseReportEmailer/CourseReportEmailer/Program.cs
@@ -1,11 +1,14 @@
 ﻿using CourseReportEmailer.Models;
 using Newtonsoft.Json;
 using System;
+using System.Data;
 
 namespace CourseReportEmailer
 {
     class Program
     {
+        public static DataColumn DataColumn { get; private set; }
+
         static void Main(string[] args)
         {
             EnrollmentDetailReportModel model = new EnrollmentDetailReportModel()
@@ -22,6 +25,33 @@ namespace CourseReportEmailer
             var json = JsonConvert.SerializeObject(model);
             //Visszalalakitjuk az elobbit
             EnrollmentDetailReportModel objectFromJson = (EnrollmentDetailReportModel)JsonConvert.DeserializeObject(json, typeof(EnrollmentDetailReportModel));
+
+            DataTable table = new DataTable();
+
+            DataColumn column1 = new DataColumn("EnrollmentId", typeof(int));
+            DataColumn column2 = new DataColumn("FirstName", typeof(string));
+            DataColumn column3 = new DataColumn("LastName", typeof(string));
+            DataColumn column4 = new DataColumn("CourseCode", typeof(string));
+            DataColumn column5 = new DataColumn("Description", typeof(string));
+
+            table.Columns.Add(column1);
+            table.Columns.Add(column2);
+            table.Columns.Add(column3);
+            table.Columns.Add(column4);
+            table.Columns.Add(column5);
+
+            table.Rows.Add(1, "Mark", "Hue", "CA", "Some description");
+            //table.Rows.Add(true, "Mark", 7, "CA", "Some description"); //Inkonzisztens adattipusok eseten atkonvertalja az oszlop tipusara - gondolom, ha lehet
+
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (DataColumn column in table.Columns)
+                {
+                    Console.WriteLine(row[column]);
+                }
+            }
+
+            //C# object --> Json --> DataTable
         }
     }
 }
