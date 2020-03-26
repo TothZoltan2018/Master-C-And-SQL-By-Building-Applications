bfd5ed37e8a4efff6ac7c23bd6d93cba269164df
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-03-26 07:54:36 +0100

    CourseManager - Initial setups for Caliburn.

---
+++ CourseManager/CourseManager/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using CourseManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CourseManager
+{
+    //A Caliburn-nek kell ez az allomany
+    //Nuget telepitesek: Dapper, Caliburn. 
+    //A Caliburn-nek mappakat hoztunk letre: View, ViewModels, Models
+    //Letoroljuk a MainWindow.xaml-t.
+    class Startup : BootstrapperBase
+    {
+        public Startup()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //Majd ide tesszuk a ViewModel-unket. Itt indul a programunk.
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
