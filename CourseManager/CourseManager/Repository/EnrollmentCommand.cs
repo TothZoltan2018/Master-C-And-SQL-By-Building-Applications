1977a76d19b293a4a6c0e7b50df1659d3e56bdca
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-03-27 15:14:59 +0100

    CourseManager - SQL commands handling added

---
+++ CourseManager/CourseManager/Repository/EnrollmentCommand.cs
@@ -0,0 +1,62 @@
+﻿using CourseManager.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.Repository
+{
+    class EnrollmentCommand
+    {
+        private string _connectionString;
+
+        public EnrollmentCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IList<EnrollmentModel> GetList()
+        {
+            List<EnrollmentModel> enrollments = new List<EnrollmentModel>();
+
+            var sql = "Enrollments_GetList";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                enrollments = connection.Query<EnrollmentModel>(sql).ToList();
+            }
+
+            //Beallitjuk, hogy ezek a mar elmentett adatok, nem pedig a most felvitt ujak.
+            foreach (var enrollment in enrollments)
+            {
+                enrollment.IsCommitted = true;
+            }
+
+            return enrollments;
+        }
+
+        public void Upsert(EnrollmentModel enrollmentModel)
+        {
+            var sql = "Enrollments_Upsert";
+            //Windows username
+            var userId = System.Security.Principal.WindowsIdentity.GetCurrent().Name.ToString(); //Szerintem nem kell a ToString()
+
+            //Az SQL Serveren taolt stored procedure User ddwfined Table tipust fogad. Adattablaban adjuk at neki.
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("EnrollmentId", typeof(int));
+            dataTable.Columns.Add("StudentId", typeof(int));
+            dataTable.Columns.Add("CourseId", typeof(int));
+            dataTable.Rows.Add(enrollmentModel.EnrollmentId, enrollmentModel.StudentId, enrollmentModel.CourseId);
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(sql, new { @EnrollmentType = dataTable.AsTableValuedParameter("EnrollmentType"), @UserId = userId}, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+    }
+}
